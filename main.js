5b6eb9c2fc3fc3f7c32a8615f95cd0b5492b002e
BananaJam <vladbyn@gmail.com>
2023-11-13 14:14:54 +0200

    put method fix

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({extended : false}));
 
+app.use(bodyParser.raw({ type: 'text/plain' }));
+
 app.use(multer().any());
 
 app.use("/", express.static(__dirname + '/static'));
@@ -54,7 +56,7 @@ app.put("/notes/:noteName", (req, res) =>{
   note = req.params.noteName;
   note_names = notes.map(a => a.note_name); 
   if(note_names.includes(note)){
-    notes[note_names.indexOf(note)].note = req.body.note
+    notes[note_names.indexOf(note)].note = req.body.toString()
     res.sendStatus(200)
   }else{
     res.sendStatus(404)
